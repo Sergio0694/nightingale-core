c0f90551a1ebdf8f630abdcf166b4cbb1bfb25ec
Sergio Pedri <sergio0694@live.com>
2019-12-18 17:30:51 +0100

    Initial draft of the public APIs

---
+++ Nightingale.Core/CodeGenerators/Enums/CodegenLanguage.cs
@@ -0,0 +1,13 @@
+﻿namespace Nightingale.Core.CodeGenerators.Enums
+{
+    /// <summary>
+    /// An <see langword="enum"/> that indicates a given language/framework to use to generate code for a set of requests
+    /// </summary>
+    public enum CodegenLanguage
+    {
+        /// <summary>
+        /// Codegen for the refit library, see <see href="https://github.com/reactiveui/refit"/>
+        /// </summary>
+        Refit
+    }
+}
--- Nightingale.Core/CodeGenerators/ICodeGenerator.cs
+++ Nightingale.Core/CodeGenerators/ICodeGenerator.cs
@@ -1,31 +1,27 @@
 ﻿using Nightingale.Core.Models;
-using System.Threading.Tasks;
+using Nightingale.Core.CodeGenerators.Enums;
 
 namespace Nightingale.Core.CodeGenerators
 {
     /// <summary>
-    /// Interface for generating code from workspace items.
+    /// An <see langword="interface"/> for generating code from workspace items.
     /// </summary>
     public interface ICodeGenerator
     {
         /// <summary>
-        /// Gets the programming language 
-        /// that the code generator produces.
-        /// </summary>
-        string Language { get; }
-
-        /// <summary>
         /// Generates code based on a <see cref="WorkspaceRequest"/>.
         /// </summary>
         /// <param name="request">The <see cref="WorkspaceRequest"/> to use.</param>
-        /// <returns>A string representing generated code.</returns>
-        Task<string> GenerateCodeAsync(WorkspaceRequest request);
+        /// <param name="language">The target language to use to generate code.</param>
+        /// <returns>A <see cref="string"/> representing generated code.</returns>
+        string GenerateCode(WorkspaceRequest request, CodegenLanguage language);
 
         /// <summary>
         /// Generates code based on a <see cref="WorkspaceCollection"/>.
         /// </summary>
-        /// <param name="request">The <see cref="WorkspaceCollection"/> to use.</param>
-        /// <returns>A string representing generated code.</returns>
-        Task<string> GenerateCodeAsync(WorkspaceCollection collection);
+        /// <param name="collection">The <see cref="WorkspaceCollection"/> to use.</param>
+        /// <param name="language">The target language to use to generate code.</param>
+        /// <returns>A <see cref="string"/> representing generated code.</returns>
+        string GenerateCode(WorkspaceCollection collection, CodegenLanguage language);
     }
 }
